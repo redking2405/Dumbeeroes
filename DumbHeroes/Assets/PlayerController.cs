574098ed4089b2e9c4135eb50d97b40da8b5e6df
Arnaud Cerisey <acerisey4@gmail.com>
2019-10-04 10:51:16 +0200

    Mise en place du projet

---
+++ DumbHeroes/Assets/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerController : MonoBehaviour
+{
+    public int V_playerId;
+    Player V_player;
+    Rigidbody2D rb;
+
+    [SerializeField]
+    float V_moveSpeed;
+    [SerializeField]
+    float V_armSpeed;
+    public Rigidbody2D O_Lhand,O_Rhand;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        V_player = ReInput.players.GetPlayer(V_playerId);
+    }
+    
+    void Update()
+    {
+        Vector2 aimVector = new Vector2(V_player.GetAxis("ArmX"), V_player.GetAxis("ArmY"));
+        AimArm(aimVector);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(V_player.GetAxis("MoveX")*V_moveSpeed, rb.velocity.y);
+    }
+
+    void AimArm(Vector2 direction)
+    {
+        if (direction.magnitude != 0)
+        {
+            O_Lhand.velocity = direction*V_armSpeed;
+            O_Rhand.velocity = direction* V_armSpeed;
+        }
+        if (V_player.GetButtonDown("Grab"))
+        {
+            Collider2D[] hitsL = Physics2D.OverlapCircleAll(O_Lhand.transform.position, 0.4f);
+            Collider2D[] hitsR = Physics2D.OverlapCircleAll(O_Rhand.transform.position, 0.4f);
+            foreach (Collider2D hit in hitsL)
+            {
+                if(hit.tag == "GrabAble")
+                {
+                    hit.transform.parent = O_Lhand.transform;
+                    hit.GetComponent<Rigidbody2D>().isKinematic = true;
+                    break;
+                }
+            }
+            foreach (Collider2D hit in hitsR)
+            {
+                if (hit.tag == "GrabAble")
+                {
+                    hit.transform.parent = O_Rhand.transform;
+                    hit.GetComponent<Rigidbody2D>().isKinematic = true;
+                    break;
+                }
+            }
+        }
+        if (V_player.GetButtonUp("Grab"))
+        {
+            foreach (Transform t in O_Lhand.transform)
+            {
+                t.parent = null;
+                Rigidbody2D tRb = t.GetComponent<Rigidbody2D>();
+                tRb.isKinematic = false;
+                tRb.velocity = O_Rhand.velocity;
+
+            }
+            foreach (Transform t in O_Rhand.transform)
+            {
+                t.parent = null;
+                Rigidbody2D tRb = t.GetComponent<Rigidbody2D>();
+                tRb.isKinematic = false;
+                tRb.velocity = O_Rhand.velocity;
+            }
+        }
+    }
+}
---
+++ DumbHeroes/Assets/Scripts/Scripts_Arnaud/EventOnCollision.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class EventOnCollision : MonoBehaviour
+{
+    public UnityEvent v_Event;
+    public float v_Velocity;
+    
+
+
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<Rigidbody2D>().velocity.x >= v_Velocity && collision.gameObject.GetComponent<Rigidbody2D>().velocity.y != 0)
+        {
+            v_Event.Invoke();
+        }
+       
+    }
+}
