9339a22cd51354292df54589c1fdb5648d067c76
Arnaud Cerisey <acerisey4@gmail.com>
2019-10-04 11:58:39 +0200

    Event on collision + Activate Ragdoll

---
+++ DumbHeroes/Assets/Scripts/Scripts_Arnaud/Activate_Ragdoll.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activate_Ragdoll : MonoBehaviour
+{
+    public GameObject[] v_gameObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Ragdoll()
+    {
+        for(int i=0; i<v_gameObjects.Length; i++)
+        {
+            v_gameObjects[i].GetComponent<HingeJoint2D>().enabled = true;
+            v_gameObjects[i].GetComponent<Rigidbody2D>().isKinematic = false;
+        }
+    }
+}
