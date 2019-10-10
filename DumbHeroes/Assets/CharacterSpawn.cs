5c0503030a87ae80e78e66ee5b0a8a26f89032fe
maxime blandin <maximeblandin.pro@gmail.com>
2019-10-10 15:04:33 +0200

    Multiplayer + Start ShockWave VFX

---
+++ DumbHeroes/Assets/CharacterSpawn.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class CharacterSpawn : MonoBehaviour
+{
+    public List<Player> PlayerToSpawnList= new List<Player>();
+    public GameObject PrefabPlayer;
+    public Transform SpawnerPosition;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AddToPlayers();
+
+    }
+
+    void AddToPlayers()
+    {
+        foreach (Player p in ReInput.players.AllPlayers)
+        {
+            if (!PlayerToSpawnList.Contains(p) && p.GetAnyButton())
+            {
+                PlayerToSpawnList.Add(p);
+                SpawnPlayer(p);
+            }
+        }
+        
+    }
+
+    void SpawnPlayer(Player PlayerToSpawn)
+    {
+        GameObject PlayerObject = Instantiate(PrefabPlayer, SpawnerPosition.position, Quaternion.identity);
+        PlayerObject.GetComponent<PlayerController>().V_playerId = PlayerToSpawn.id;
+    }
+}
