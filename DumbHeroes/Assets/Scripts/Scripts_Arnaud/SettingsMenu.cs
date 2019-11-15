e75cc156a5a0f5b0971592d95d0d15c487f0c727
Arnaud Cerisey <acerisey4@gmail.com>
2019-11-15 10:59:53 +0100

    OPtion Menus

---
+++ DumbHeroes/Assets/Scripts/Scripts_Arnaud/SettingsMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer v_AudioMixer;
+    public AudioMixerGroup v_SFX;
+    public AudioMixerGroup v_Music;
+    public Slider masterVolume;
+    public Slider sfVolume;
+    public Slider muVolume;
+
+
+    private void Start()
+    {
+        masterVolume.onValueChanged.AddListener(delegate { SetVolume(); });
+        muVolume.onValueChanged.AddListener(delegate { SetMusic(); });
+        sfVolume.onValueChanged.AddListener(delegate { SetSFX(); });
+    }
+
+    public void SetVolume()
+    {
+        v_AudioMixer.SetFloat("volume", masterVolume.value);
+    }
+
+    public void SetSFX()
+    {
+        v_SFX.audioMixer.SetFloat("sfVolume", sfVolume.value);
+    }
+
+    public void SetMusic()
+    {
+        v_Music.audioMixer.SetFloat("muVolume", muVolume.value);
+    }
+}
