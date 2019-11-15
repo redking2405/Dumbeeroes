8c5bcc75fcdfae3c6b1e55ed798890b3e25f26a2
Elliot <elliotblazy2011@gmail.com>
2019-11-15 08:46:46 +0100

    Controller verions

---
+++ DumbHeroes/Assets/Scripts/ThrownObjectEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrownObjectEffect : MonoBehaviour
+{
+    public ParticleSystem collparticles;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        foreach(ContactPoint2D p in collision.contacts)
+        {
+            collparticles.transform.position = p.point;
+            collparticles.transform.rotation = Quaternion.FromToRotation(collparticles.transform.up, p.normal) * collparticles.transform.rotation;
+            collparticles.Play();
+        }
+
+    }
+}
