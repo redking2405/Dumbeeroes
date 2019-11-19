b91a08b84fdb4a0331c18ec1bd09d27cd6aacaf9
Arnaud Cerisey <acerisey4@gmail.com>
2019-11-19 09:10:45 +0100

    Canon First Version

--- DumbHeroes/Assets/Scripts/Scripts_Arnaud/Canon.cs
+++ DumbHeroes/Assets/Scripts/Scripts_Arnaud/Canon.cs
@@ -6,21 +6,81 @@ public class Canon : ActivableObjects
 {
     public float v_Angle;
     public float v_Force;
-    Transform v_Position;
-    float v_TimerMax;
+    [SerializeField] Transform v_Position;
+    [SerializeField] float v_TimerMax;
     float v_Timer;
     GameObject v_ToLaunch;
+    [SerializeField] GameObject v_PrefabCanonBall;
     bool v_Launched;
-    bool v_Loaded;
+    bool v_Ready;
+    public bool v_Loaded;
+    bool canShoot;
     // Start is called before the first frame update
     void Start()
     {
-        
+        v_Timer = v_TimerMax;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (canShoot)
+        {
+            if (!v_Ready)
+            {
+                v_Timer -= Time.deltaTime;
+
+                if (v_Timer <= 0)
+                {
+                    v_Ready = true;
+                    v_Launched = false;
+                }
+            }
+
+            if (v_Ready && !v_Launched)
+            {
+                Shoot();
+            }
+        }
         
+    }
+
+    public override void Activate()
+    {
+        canShoot = true;
+        base.Activate();
+    }
+
+
+    public override void Deactivate()
+    {
+        canShoot = false;
+        base.Deactivate();
+    }
+    public void Shoot()
+    {
+        v_Timer = v_TimerMax;
+        if (v_Loaded)
+        {
+
+        }
+
+        else
+        {
+            GameObject canonBall = Instantiate(v_PrefabCanonBall, v_Position.position, Quaternion.identity);
+            Rigidbody2D rbd = canonBall.GetComponent<Rigidbody2D>();
+            if (rbd)
+            {
+                v_Angle = Mathf.Deg2Rad * v_Angle;
+                Vector2 direction = Quaternion.Euler(0, 0, v_Angle)*Vector2.right;
+                rbd.AddForce(direction * v_Force, ForceMode2D.Impulse);
+
+            }
+
+            
+        }
+
+        v_Launched = true;
+        v_Ready = false;
     }
 }
