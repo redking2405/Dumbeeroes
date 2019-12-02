34dd23b445f670fc024bb4f5335f7f45a09ad814
maxime blandin <maximeblandin.pro@gmail.com>
2019-12-02 10:53:44 +0100

    Camera Script - DONE

--- DumbHeroes/Assets/CameraController.cs
+++ DumbHeroes/Assets/CameraController.cs
@@ -12,28 +12,41 @@ public class CameraController : MonoBehaviour
 
     public Camera cam;
     public BoxCollider2D Bounds;
+    Vector2 origin;
+
     float edgeSize = 100f;
     float moveSpeed = 7f;
     float zoomSpeed = 0.5f;
+
+    float vertExtent;
+    float horzExtent;
+
 
     // Start is called before the first frame update
     void Start()
     {
         cameraPoint = transform.position;
         zoom = cam.orthographicSize;
+
+        origin = Bounds.transform.position;
+         
+        vertExtent = cam.orthographicSize;
+        horzExtent = vertExtent * Screen.width / Screen.height;
     }
     // Update is called once per frame
     void Update()
     {
+        vertExtent = zoom;
+        horzExtent = vertExtent * Screen.width / Screen.height;
 
-        float vertExtent = cam.orthographicSize;
-        float horzExtent = vertExtent * Screen.width / Screen.height;
-        /*
-        cameraPoint.x = Mathf.Clamp(cameraPoint.x, -Bounds.size.x / 2 + horzExtent, Bounds.size.x / 2 - horzExtent);
-        cameraPoint.y = Mathf.Clamp(cameraPoint.y, -Bounds.size.y / 2 + vertExtent, Bounds.size.y / 2 - vertExtent);
-        */
         cameraPoint = GetMidPointPlayers();
-        zoom = Mathf.Clamp(zoom, 5, 15);
+
+        cameraPoint.x = Mathf.Clamp(cameraPoint.x, origin.x-Bounds.size.x / 2 + horzExtent, origin.x+Bounds.size.x / 2 - horzExtent);
+        cameraPoint.y = Mathf.Clamp(cameraPoint.y, origin.y-Bounds.size.y / 2 + vertExtent, origin.y+Bounds.size.y / 2 - vertExtent);
+        
+
+        zoom = GetFurthestDistanceBetweenPlayers();
+        zoom = Mathf.Clamp(zoom, 10, Mathf.Min(20,Bounds.size.y/2,Bounds.size.x/2));
 
         float targetzoom = Mathf.Lerp(cam.orthographicSize, zoom, Time.deltaTime * zoomDamp);
         cam.orthographicSize = targetzoom;
@@ -56,5 +69,29 @@ public class CameraController : MonoBehaviour
         }
         
         return midpoint;
+    }
+
+    float GetFurthestDistanceBetweenPlayers()
+    {
+        float furthestDistance = 5;
+
+        foreach (PlayerData pd1 in CharacterSpawn.PlayerSpawnedList)
+        {
+            foreach (PlayerData pd2 in CharacterSpawn.PlayerSpawnedList)
+            {
+                if(pd1!=pd2)
+                {
+                    Vector2 player1Pos = pd1.playerObject.transform.position;
+                    Vector2 player2Pos = pd2.playerObject.transform.position;
+
+                    if(Vector2.Distance(player1Pos,player2Pos)>furthestDistance)
+                    {
+                        furthestDistance = Vector2.Distance(player1Pos, player2Pos);
+                    }
+                }
+            }
+        }
+
+        return furthestDistance;
     }
 }
